fdefe228c1c49ad2bf0c9f4ef0faa80c2b88ce9c
Sudipto Das <98449874+Sudip200@users.noreply.github.com>
2023-06-21 10:49:11 +0000

    removing script

---
+++ app/src/main/java/com/js/sds/DelegationService.java
@@ -0,0 +1,14 @@
+package com.js.sds;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
